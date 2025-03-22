a9117e8634ea4a4d2ee435b1f80fb34862b4c3dc
Hamzat Victor Oluwabori <oluwaborihamzat@gmail.com>
2025-03-22 18:21:07 +0200

    feat: add attribution

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export default function App() {
     <main className="bg-gray-100 min-h-screen flex items-center justify-center">
       <div className="bg-white p-8 rounded shadow text-center">
         <h1 className="text-2xl font-bold mb-4">
-          Welcome to my ⚛️ React app built with ⚡ Vite!
+          Welcome to my ⚛️ React app built with ⚡ Vite & Sandbox!
         </h1>
         <p className="text-gray-600 mb-2">This is a tailwind template.</p>
         <Button onClick={() => setCount((count) => count + 1)}>
